6a2cde73006d408c70519f45509d1fc29f65adcd
Debmalya Jash (deb) <debmalya.jash.e@thalesdigital.io>
2020-05-26 21:20:26 +0800

    Adding account based search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- src/main/java/org/deb/account/exclusion/repository/AccountRepository.java
+++ src/main/java/org/deb/account/exclusion/repository/AccountRepository.java
@@ -1,7 +1,11 @@
 package org.deb.account.exclusion.repository;
 
 import org.deb.account.exclusion.entity.ExclusionAccounts;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface AccountRepository extends JpaRepository<ExclusionAccounts,String> {
+  Page<ExclusionAccounts> findByAccountNumberContaining(@RequestParam("accountNumber") String accountNumber, Pageable pageable);
 }
